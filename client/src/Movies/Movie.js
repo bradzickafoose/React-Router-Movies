63fbfdbf4cee2d542a85eeb675f9dd6ad06ea06f
Brad <brad@viziworx.com>
2019-10-14 17:45:42 -0400

    Updated typo in component and stars.map

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,14 +5,15 @@ const Movie = (props) => {
   const [movie, setMovie] = useState({});
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
        axios
-        .get(`http://localhost:5000/api/movies/${movie.id}`)
+        .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
+          console.log(response.data);
         })
         .catch(error => {
           console.error(error);
@@ -43,7 +44,7 @@ const Movie = (props) => {
         </div>
         <h3>Actors</h3>
 
-        {stars.map(star => (
+        {stars && stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
